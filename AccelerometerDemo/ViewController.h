9e2185310c9ea13658c371c9849868f3620b8fc5
Omar Faruqe <faruqe.cse@gmail.com>
2016-03-29 23:53:44 -0400

    Initial Commit

---
+++ AccelerometerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AccelerometerDemo
+//
+//  Created by Omar Faruqe on 2016-03-29.
+//  Copyright © 2016 Omar Faruqe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
